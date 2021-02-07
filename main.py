9167174c766365d839066d0c1f22d2c131d87eed
Nicholas784 <nicholasdeane.nd@gmail.com>
2021-02-07 02:14:48 -0500

    Second Commit-Tank Data

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from datetime import datetime 
 from flask_cors import CORS
 
-
-dt = datetime.now()
 
 app = Flask(__name__)
 CORS(app) 
@@ -24,19 +22,22 @@ profile = {
     }
 }
 
+tank_info = []
+tank_id = 0
+
 class Profile(Resource):
     def get(self):
         return profile
 
     def post(self):
-        profile["data"]["last_updated"] = dt.strftime("%c")
+        profile["data"]["last_updated"] = datetime.now().strftime("%c")
         profile["data"]["username"] = request.form['username']
         profile["data"]["role"] = request.form['role']
         profile["data"]["color"] = request.form['color']
         return profile
 
     def patch(self):
-        profile["data"]["last_updated"] = dt.strftime("%c")
+        profile["data"]["last_updated"] = datetime.now().strftime("%c")
 
         data = (request.form)
         for key in data:
@@ -44,13 +45,52 @@ class Profile(Resource):
         
         return profile
 
+class Data(Resource):
+    def get(self):
+        return jsonify(tank_info)
+
+
+    def post(self):
+        global tank_id
+        tank_id += 1
+        tank = {}
+
+        tank["id"] = tank_id
+        tank["location"] = request.form['location']
+        tank["lat"] = request.form['lat']
+        tank["long"] = request.form['long']
+        tank["percentage_full"] = request.form['percentage_full']
+
+        tank_info.append(tank)
+        return jsonify(tank_info)
+
+
+class Data2(Resource):
+    def patch(self, tank_id):
+        for item in tank_info: 
+            if item["id"] == tank_id:
+                data = (request.form)
+                for key in data:
+                    item[key] = request.form[key]
+
+        return jsonify(tank_info)
+
+    def delete(self, tank_id):
+        for item in tank_info:
+            if item["id"] == tank_id:
+                tank_info.remove(item)
+
+        return {"success": True}
+
 
 api.add_resource(Profile, "/profile")
+api.add_resource(Data, "/data")
+api.add_resource(Data2, "/data/<int:tank_id>")
 
 if __name__ == "__main__":
     app.run(
         debug=True,
-        #port = 3000,
-        #host = "0.0.0.0"
+        port = 3000,
+        host = "0.0.0.0"
     )
     
